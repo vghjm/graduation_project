b3afcbb39f2aa28261be6fce8226923f61989cc8
hhssung <77eric@naver.com>
2020-09-22 19:21:56 -0700

    0923 add darknet

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
+const { Darknet } = require('darknet');
+
 var express = require('express');
 var router = express.Router();
+
+// Init
+const darknet = new Darknet({
+    weights: 'darknet_files/yolov3-tiny.weights',
+    config: 'darknet_files/yolov3-tiny.cfg',
+    namefile: 'darknet_files/coco.names'
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* TEST darknet */
+router.get('/darknet', function(req, res) {
+  let test = darknet.detect('dog.jpg');
+  res.json({res: test});
 });
 
 module.exports = router;
