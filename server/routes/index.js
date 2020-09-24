e5be5b977e2087f3781824e5b3a7b74f84d50a55
hhssung <77eric@naver.com>
2020-09-24 02:08:38 -0700

    0924 server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const { Darknet } = require('darknet');
+const multer = require('multer');
 
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 // Init
 const darknet = new Darknet({
@@ -10,15 +12,35 @@ const darknet = new Darknet({
     namefile: 'darknet_files/coco.names'
 });
 
+var _storage = multer.diskStorage({
+    // upload 폴더에 저장
+    destination: function (req, file, cb) { 
+        cb(null, 'image/')
+    },
+    // 저장할 파일 이름 정하기
+    filename: function (req, file, cb) {
+        // orignalname.jpg 형식으로 저장
+        cb(null, file.originalname) 
+    }
+  });
+
+var upload = multer({ storage : _storage });
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 /* TEST darknet */
-router.get('/darknet', function(req, res) {
-  let test = darknet.detect('dog.jpg');
-  res.json({res: test});
+router.post('/upload', upload.single('pngfile'), function(req, res) {
+ 
+  let file = req.file;  
+  
+  let dark = darknet.detect('image/'+file.originalname);
+  console.log(dark);
+   res.json(dark);
+
 });
 
 module.exports = router;
